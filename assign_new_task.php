8bd9a155ba6b43417ac67c3ea29c7d67a8362c14
Trisha <tatolentino@student.apc.edu.ph>
2022-09-08 04:08:59 +0800

    Form changes

--- assign_new_task.php
+++ assign_new_task.php
@@ -24,7 +24,7 @@
 ?>
 	<div class="container-fluid py-4" id="main_content">
         <div class="info">
-					<form action="backend/create_task.p.php" method="post">
+					<form class="needs-validation" action="backend/create_task.p.php" method="post" novalidate>
 						<?php
 						
 							if(isset($_GET['asset']) && isset($_GET['machine']) && isset($_GET['e_id']) && isset($_GET['room'])){
@@ -54,18 +54,24 @@
 								<div class="form-group">
 								<h2>Machine Details</h2>
 								<hr class="rounded">
-								<label>Type of Machine</label>
-								<select class="form-control" name="typeOfMachine" id="typeOfMachine">
-									<option value="none">--</option>
+								<label>Choose the type of Machine</label>
+								<select class="form-control" name="typeOfMachine" id="typeOfMachine" required>
+									<option value="">--</option>
 									<?php
 										include 'backend/get_asset.p.php';
 									?>
 								</select>
+								<div class="invalid-feedback">
+									Please choose from the list
+								</div>
 								</div>
 
 								<div class="form-group">
-									<label for="formGroupExampleInput2">Machine</label>
-									<input type="text" class="form-control" id="machine" name="machine"placeholder="Select Machine" disabled>
+									<label for="formGroupExampleInput2">Name of the Machine</label>
+									<input type="text" class="form-control" id="machine" name="machine"placeholder="Select Machine" disabled required>
+									<div class="invalid-feedback">
+										Please fill in this field
+									</div>
 									<select class="form-control" name="airconForm" id="HVAC">
 									  <option value="">--</option>
 										<?php
@@ -86,15 +92,18 @@
 						<h2>Task Details</h2>
 						<hr class="rounded">
 						<div class="form-group">
-							<label for="formGroupExampleInput2">Task</label>
-							<input type="text" class="form-control" name="task" placeholder="Task">
+							<label for="formGroupExampleInput2">What is the task?</label>
+							<input type="text" class="form-control" name="task" placeholder="E.g. Check capacitor" required>
+							<div class="invalid-feedback">
+								Please fill in this field
+							</div>
 						</div>
 						<div class="form-group">
-							<label for="formGroupExampleInput2">Task Description</label>
-							<input type="text" class="form-control" name="taskDesc" placeholder="Task Description">
+							<label for="formGroupExampleInput2">Task Description (optional)</label>
+							<input type="text" class="form-control" name="taskDesc" placeholder="Describe what the task is all about">
 						</div>
 						<div class="form-group">
-							<label for="typeOfForm">Assign To</label>
+							<label for="typeOfForm">Assign the task to</label>
 							<?php
 								if(isset($_GET['u_id']) && isset($_GET['username'])){
 									?>
@@ -104,21 +113,28 @@
 									<?php
 								}else{
 							?>
-							  <select class="form-control" name="assignedTo">
+							  <select class="form-control" name="assignedTo" required>
 								  <option value="">--</option>
 									<?php
 									include 'backend/get_technicians.p.php';
 									?>
 							  </select>
+							  <div class="invalid-feedback">
+								Please choose from the list
+							 </div>
 							<?php
 								}
 							?>
 						</div>
 						<div class="form-group">
-							<label for="formGroupExampleInput2">Due date & time</label>
-							<input type="datetime-local" class="form-control" name="dueDate">
+							<label for="formGroupExampleInput2">Due date</label>
+							<input type="date" class="form-control" name="dueDate" required>
+							<div class="invalid-feedback">
+								Please fill in this field
+							 </div>
 						</div> 
-						<button class="btn btn-primary mb-2" type="submit" name="submit">Submit</button>
+						<button class="btn btn-primary mb-2" type="submit" name="submit" onclick="alert('Are you sure you want to submit?')">Submit</button>
+						<button type="reset" class="btn btn-danger mb-2" onclick="alert('Are you sure you want to reset?')">Reset</button>
 					</form>
 				</div>
     
@@ -146,5 +162,29 @@
 			}
 		});
 		$("#typeOfMachine").trigger("change");
+	</script>
+
+	<!-- script for field validations -->
+	<script type="text/javascript">
+		// Example starter JavaScript for disabling form submissions if there are invalid fields
+		(function () {
+		  'use strict'
+
+		  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+		  var forms = document.querySelectorAll('.needs-validation')
+
+		  // Loop over them and prevent submission
+		  Array.prototype.slice.call(forms)
+		    .forEach(function (form) {
+		      form.addEventListener('submit', function (event) {
+		        if (!form.checkValidity()) {
+		          event.preventDefault()
+		          event.stopPropagation()
+		        }
+
+		        form.classList.add('was-validated')
+		      }, false)
+		    })
+		})()
 	</script>
 	</div> 
