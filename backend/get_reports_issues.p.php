750ac65028ddc33e43f684a8d72ec9891016abc4
JasfferTP <jasfferp@gmail.com>
2022-09-22 14:53:47 +0800

    updated technician tables

--- backend/get_reports_issues.p.php
+++ backend/get_reports_issues.p.php
@@ -11,14 +11,12 @@
 		$sql = "SELECT report_id, reports.machine_id, reports.task, reports.date_created, reports.task_due, reports.date_submitted, reports.assigned_user, reports.report_status, equipment.equipment_id, equipment.equipment_name, equipment.location_id, location.location_id, location.floor,location.room_number, users.users_id, users.username 
 		FROM `reports`, `users`,`location`,`equipment` 
 		WHERE users.users_id = ".$_SESSION['userId']." AND reports.assigned_user = ".$_SESSION['userId']." AND equipment.equipment_id = reports.machine_id AND equipment.location_id = location.location_id AND reports.report_status = 'done'
-		ORDER BY reports.date_submitted DESC 
-		LIMIT ".$min.", 10";
+		ORDER BY reports.date_submitted DESC";
 	}else if($_GET['site'] == "My Issues Reported"){
 		$sql = "SELECT issue.issue_id, issue.machine_id, issue.issue, issue.issue_status, issue.assigned_to, issue.date_created, issue.date_due, issue.date_issue_resolved, equipment.equipment_id, equipment.equipment_name, equipment.location_id, location.location_id, location.floor,location.room_number, users.users_id, users.username, issue.assigned_to 
 		FROM `issue`, `users`,`location`,`equipment` 
 		WHERE users.users_id = ".$_SESSION['userId']." AND issue.submitted_by = ".$_SESSION['userId']." AND equipment.equipment_id = issue.machine_id AND equipment.location_id = location.location_id
-		ORDER BY issue.date_created DESC 
-		LIMIT ".$min.", 10 ";
+		ORDER BY issue.date_created DESC";
 	}
 	
 	$stmt = mysqli_stmt_init($conn);
@@ -47,7 +45,13 @@
 			}else{
 			?>
 				<tr>
-					<td colspan="7" class="text-center"> There are no reports submitted by you yet.</td>
+					<td class="text-center"> </td>
+					<td class="text-center"> </td>
+					<td class="text-center"> </td>
+					<td class="text-center"> There are no reports submitted by you yet.</td>
+					<td class="text-center"> </td>
+					<td class="text-center"> </td>
+					<td class="text-center"> </td>
 				</tr>
 			<?php
 			}
@@ -115,7 +119,13 @@
 			}else{
 			?>
 				<tr>
-					<td colspan="7" class="text-center"> There are no issues submitted by you yet.</td>
+					<td class="text-center"> </td>
+					<td class="text-center"> </td>
+					<td class="text-center"> </td>
+					<td class="text-center"> There are no issues submitted by you yet.</td>
+					<td class="text-center"> </td>
+					<td class="text-center"> </td>
+					<td class="text-center"> </td>
 				</tr>
 			<?php
 			}
--- technician_reports.php
+++ technician_reports.php
@@ -32,35 +32,35 @@
 	-->
 	<?php
 	if($_GET['site'] == "My Reports"){
-		?>
+    ?>
 		<a href="assign_issue.php?site=Report%20Equipment%20Issue" type="button" class="btn btn-danger btn-lg my-2">Report an equipment issue</a>
-		<table class="table rounded-3 shadow-lg table-hover mb-5">
-			<thead class="thead-dark">
-				<tr role="button" data-href="viewPastReports.php?r=<?php echo $row['report_id'];?>&e=<?php echo $row['machine_id'];?>&site=My%20Past%20Reports">
-				<th scope="col">Tasks</th>
-				<th scope="col">Equipment</th>
-				<th scope="col">Floor</th>
-				<th scope="col">Room</th>
-				<th scope="col">Date created</th>
-				<th scope="col">Due date</th>
-				<th scope="col">Date submitted</th>
-				<th scope="col">Status</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
+    <table  id="reports_table">
+        <thead >
+            <tr role="button" data-href="viewPastReports.php?r=<?php echo $row['report_id'];?>&e=<?php echo $row['machine_id'];?>&site=My%20Past%20Reports">
+                <th scope="col">Tasks</th>
+                <th scope="col">Equipment</th>
+                <th scope="col">Floor</th>
+                <th scope="col">Room</th>
+                <th scope="col">Date created</th>
+                <th scope="col">Due date</th>
+                <th scope="col">Date submitted</th>
+                <th scope="col">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
 					include 'backend/get_reports_issues.p.php';
-				?>
-			</tbody>
-		</table>
+            ?>
+        </tbody>
+    </table>
 		<?php
-			include 'backend/reports_issues_pagination.p.php';
+			//include 'backend/reports_issues_pagination.p.php';
 		?>
 		<?php
 	}else if($_GET['site'] == "My Issues Reported"){
 		?>
 		<a href="assign_issue.php?site=Report%20Equipment%20Issue" type="button" class="btn btn-danger btn-lg my-2">Report an equipment issue</a>
-		<table class="table rounded-3 shadow-lg table-hover mb-5">
+		<table id="issues_table">
 			<thead class="thead-dark">
 				<tr>
 				<th scope="col">Issues</th>
@@ -79,11 +79,11 @@
 			</tbody>
 		</table>
 		<?php
-			include 'backend/reports_issues_pagination.p.php';
+			//include 'backend/reports_issues_pagination.p.php';
 		?>
 		<?php
 	}
-	?>
+        ?>
 	
 </div>
 
@@ -95,4 +95,85 @@
 			alert.style.display = 'none';
 		}, 3000)
 	});
+</script>
+
+
+<script src="tablefilter/tablefilter.js"></script>
+
+<script data-config>
+	var filtersConfig = {
+		base_path: 'tablefilter/',
+		responsive: true,
+		paging: {
+          results_per_page: ['Records: ', [10, 25, 50, 100]]
+        },
+		col_2: 'select',
+		col_4: 'select',
+		col_7: 'select',
+		col_8: 'select',
+		alternate_rows: true,
+		rows_counter: true,
+		sticky_headers: true,
+		btn_reset: true,
+		loader: true,
+		status_bar: true,
+		mark_active_columns: true,
+		highlight_keywords: true,
+
+		col_types: ['string',
+					'string',
+					'string',
+					'string',
+					'string',
+					{ type: 'date', locale: 'en', format: '{dd}-{MM}-{yyyy|yy}' },
+					{ type: 'date', locale: 'en', format: '{dd}-{MM}-{yyyy|yy}' },
+					'string',
+					'string'
+		],
+		watermark: ['(e.g. Not functioning)', '(e.g. Generator Set 1)', '', '404-A', '','(e.g. >2022-01-01)', '(e.g. >2022-01-01)', '',''],
+		msg_filter: 'Filtering...',
+        extensions:[{ name: 'sort' }]
+	};
+
+	var tf = new TableFilter('reports_table', filtersConfig);
+    tf.init();
+</script>
+
+<script data-config>
+	var filtersConfig = {
+		base_path: 'tablefilter/',
+		responsive: true,
+		paging: {
+          results_per_page: ['Records: ', [10, 25, 50, 100]]
+        },
+		col_2: 'select',
+		col_4: 'select',
+		col_7: 'select',
+		col_8: 'select',
+		alternate_rows: true,
+		rows_counter: true,
+		sticky_headers: true,
+		btn_reset: true,
+		loader: true,
+		status_bar: true,
+		mark_active_columns: true,
+		highlight_keywords: true,
+
+		col_types: ['string',
+					'string',
+					'string',
+					'string',
+					'string',
+					{ type: 'date', locale: 'en', format: '{dd}-{MM}-{yyyy|yy}' },
+					{ type: 'date', locale: 'en', format: '{dd}-{MM}-{yyyy|yy}' },
+					'string',
+					'string'
+		],
+		watermark: ['(e.g. Not functioning)', '(e.g. Generator Set 1)', '', '404-A', '','(e.g. >2022-01-01)', '(e.g. >2022-01-01)', '',''],
+		msg_filter: 'Filtering...',
+        extensions:[{ name: 'sort' }]
+	};
+
+	var tf = new TableFilter('issues_table', filtersConfig);
+    tf.init();
 </script>
