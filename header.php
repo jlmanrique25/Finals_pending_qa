b0362e95794651b3a4b95c16615c43bd22e12ac3
JasfferTP <jasfferp@gmail.com>
2022-10-22 22:16:57 +0800

    fixed web logo

--- header.php
+++ header.php
@@ -21,6 +21,7 @@
 	<link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	
+	<link rel="icon" href="images/keoms_logo.png" />
 	<link href="style.css" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="tablefilter/style/tablefilter.css" />
 </head>
