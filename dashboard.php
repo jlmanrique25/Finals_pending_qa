bcd147c240e5bebe2c534a0ac849dc0bed287524
JasfferTP <jasfferp@gmail.com>
2022-11-06 22:00:28 +0800

    header fix 4

--- dashboard.php
+++ dashboard.php
@@ -94,12 +94,6 @@ include 'header.php';
 		  <div class="my-3 p-3 bg-body rounded shadow-sm">
 			<h6 class="border-bottom pb-2 mb-0">Activities (last 7 days)</h6>
 			
-			<!--
-			!!!!!!
-			WHERE YOU'LL INSERT THE FOR LOOP TO PRINT MULTIPLE UPDATES
-			!!!!!!
-			-->
-			
 			<?php
 				include 'backend/filter_report_issues.p.php';
 			?>
@@ -110,12 +104,12 @@ include 'header.php';
                 ?>
 			</small>
 		  </div>
-
-		</div>
-
-
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+		</div>
+
+
+	
 </body>
