2bc83188f4a611712d8d471ef544915d7c7803a9
Jan Laurene Manrique <56018395+jlmanrique25@users.noreply.github.com>
2022-11-07 12:15:46 +0800

    Update dbh.p.php

--- backend/dbh.p.php
+++ backend/dbh.p.php
@@ -1,9 +1,9 @@
 <?php
 //setting database connections
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "final_pending";
+$servername = "us-cdbr-east-06.cleardb.net";
+$username = "ba8ea00b7249f4";
+$password = "52934ed3";
+$db = "heroku_67414314b729d48";
 
 //establishing a database connection
 $conn = new mysqli($servername, $username, $password, $db);
